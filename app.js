fd023706d38e2800c563da90cbd98e61fc5dc7b8
PaolaFU <32306054+PaolaFU@users.noreply.github.com>
2017-10-09 22:53:18 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var nombre;
+var edad;
+//e puede seguir agregando variables
+ nombre = prompt("Ingresa tu nombre ");
+ edad = prompt("Ingresa tu edad");
+//console.log("Tu nombre es:"+nombre);
+//alert(nombre);
+  // console y alert sirve estan en comillas xq usare solo document.write
+document.write(nombre);
+
+document.write(edad);
